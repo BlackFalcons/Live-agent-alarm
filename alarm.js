dc30e0c59351234eb8ec8089c4dd0b77a7182a0a
Falcon <37326776+BlackFalcons@users.noreply.github.com>
2021-04-24 23:30:40 +0200

    Replace functions with a class

--- alarm.js
+++ alarm.js
@@ -1,25 +1,27 @@
-function playAlarm() {
-    alarm.currentTime = 0
-    alarm.play()
+class Alarm {
+    constructor(alarmAudio) {
+        this.alarmAudio = alarmAudio
+    }
+
+    stopAlarm() {
+        this.alarmAudio.pause()
+    }
+    
+    playAlarm(time) {
+        this.alarmAudio.currentTime = 0
+        this.alarmAudio.play()
+        setTimeout(this.stopAlarm.bind(this), time) // 16000 is about one cycle.
+    }
 }
 
-function stopAlarm() {
-    alarm.pause()
-}
-
-
-function alarmCallback() {
-    playAlarm()
-    setTimeout(stopAlarm, 16000)
-}
-
-const alarm = new Audio('https://upload.wikimedia.org/wikipedia/commons/f/f9/Motorsirene_-_Feuerwehralarm.ogg')
+const flightAlarm = new Audio('https://upload.wikimedia.org/wikipedia/commons/f/f9/Motorsirene_-_Feuerwehralarm.ogg')
+const alarm = new Alarm(flightAlarm)
 const serviceStatusText = document.querySelector(".ServiceStatusText")
 
 const e = document.querySelector(".TodoNotification")
 const observer = new MutationObserver(function (event) {
     if (e.classList.contains("TodoNotification-TicketToDo")) {
-        alarmCallback()
+        alarm.playAlarm(16000)
     }
 })
 
